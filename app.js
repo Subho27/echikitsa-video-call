9159f249ab6e739fc68074f47f1104d2bea150c1
Subhodip Rudra <42699151+Subho27@users.noreply.github.com>
2024-04-04 15:46:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -394,7 +394,7 @@ const createWebRtcTransport = async (router) => {
         const webRtcTransport_options = {
           listenIps: [
             {
-              ip: '192.168.0.107', // private/public ip
+              ip: '0.0.0.0', // private/public ip
               // announcedIp: '192.168.0.107', 
             }
           ],
